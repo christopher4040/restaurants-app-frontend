b32e213bd63d6ee062304f2cc8df44586a599985
Christopher Maldonado <christopherpr101@gmail.com>
2023-08-05 15:25:31 -0500

    Update - .env file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0566e89cc482a170a2dc";
+/******/ 	var hotCurrentHash = "21b3a49c4badbb00dda0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
